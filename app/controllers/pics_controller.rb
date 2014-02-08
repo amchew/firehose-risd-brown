373176b14f3d82863a4c5956fc39dcf8acc535ef
Amanda Chew <amandachew@amandas-mbp.risd.edu>
2014-02-08 17:11:04 -0500

    add form and transfer information

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -5,4 +5,20 @@ class PicsController < ApplicationController
 		@pics = Pic.all;
 	end
 
+		# to make one new pic
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		Pic.create(pics_params)
+		redirect_to pics_path
+	end
+
+	# this is how to suck the params out when someone presses the submit button
+	private
+
+	def pics_params
+		params.require(:pic).permit(:hour,:emotion,:learned)
+	end
 end
